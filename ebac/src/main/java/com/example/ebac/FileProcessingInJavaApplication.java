18d989cc3501f1045988a5e95a3ef94d99cf5885
Ana Teixeira <160668815+raizen-anateixeira@users.noreply.github.com>
2025-02-24 12:30:02 -0300

    Iniciando o projeto

---
+++ ebac/src/main/java/com/example/ebac/FileProcessingInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessingInJavaApplication.class, args);
+	}
+
+}
---
+++ ebac/src/test/java/com/example/ebac/FileProcessingInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
